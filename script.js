7ad12aab7847016641eef011395c6882d4b0932a
lanangwisana <lanangwisana@gmail.com>
2024-04-05 15:37:06 +0700

    v2.1 add function cekFrom

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let buku = [
   { nama: "lupin", jumlah: 2 },
   { nama: "aladin", jumlah: 2 },
 ];
+
+function cekForm() {
+  const jumlah = persenInt(document.getElementById("jumlah").value);
+  const pilihBuku = document.getElementById("pilih-buku").value;
+}
