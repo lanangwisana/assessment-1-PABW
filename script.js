4ca3adbb9a36b34d9e346ff20ce71cca5d655acf
lanangwisana <lanangwisana@gmail.com>
2024-04-05 15:42:51 +0700

    v.2.1 add decision apabila jumlah dan pilihBuku nilainya kosong atau tidak memilih maka akan muncul alert

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ let buku = [
 function cekForm() {
   const jumlah = persenInt(document.getElementById("jumlah").value);
   const pilihBuku = document.getElementById("pilih-buku").value;
+
+  if (jumlah === 0) {
+    alert("Jumlah buku harus lebih dari 0");
+    return false;
+  }
+  if (pilihBuku === "") {
+    alert("Pilihan buku tidak boleh kosong");
+    return false;
+  }
 }
